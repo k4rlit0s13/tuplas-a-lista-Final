08f45062aaad0a30ccca7ee8cab20f8b243d5c87
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-02-28 09:55:05 -0500

    Final

--- Catalogo de Panaderia(Final).py
+++ Catalogo de Panaderia(Final).py
@@ -20,7 +20,7 @@ if option == 0:
     for i, val in enumerate(ProductosDulces):
         print(f"{i}. {val} ${ValorDulce[i]}")
 
-    opcion = int(input("Seleccione un producto (ingrese el número correspondiente): "))
+    opcion = int(input("Seleccione un producto (número): "))
     cantidad = int(input("Ingrese la cantidad que desea comprar: "))
     total = ValorDulce[opcion] * cantidad
 
@@ -49,6 +49,23 @@ if option == 0:
 if option==1:
     Productosal=["Baguettes"",Pan de masa madre","Pan de ajo","ocaccia","Pretzels","Bollos de queso","Empanadas","Pan de centeno","Pan de aceitunas,Pan de hierbas"]
     valoressal=[5000,6000,4000,8000,4500,2500,2000,5500,4500,4000]
+    
+    for i, val in enumerate(Productosal):
+        print(f"{i}. {val} ${valoressal[i]}")
+
+    opcion=int(input("Seleccione un producto(numero):"))
+    cantidad=int(input("Ingrese la cantidad que desea comprar: "))
+    total=valoressal[opcion]*cantidad
+
+    print(f"Usted seleccionó el producto {Productosal[opcion]} con un valor de ${total}")
+
+    dinero = int(input("Ingrese la cantidad de dinero disponible: "))
+    vuelto = dinero - total
+    
+    if dinero >= total:
+        print(f"Usted compró el producto {Productosal[opcion]} con un valor de ${total}, sus vueltos son ${vuelto}")
+    else:
+        print(f"Le falta dinero, necesita ${-vuelto} más para comprar este producto.")
 
 
 
@@ -68,6 +85,24 @@ if option==1:
 if option==2:
     Productospastel=["Torta de chocolate","Tarta de manzana","Torta de zanahoria (Carrot cake)","Torta Red Velvet","Tarta de limón","Torta de queso (Cheesecake)","Torta de frutas","Tarta Sacher (Sachertorte)","Torta de tres leches","Torta de nueces y caramelo (Sticky toffee cake)"]
     valorespastel=[40000,35000,45000,50000,40000,50000,45000,55000,40000,50000]
+    
+    for i, val in enumerate(Productospastel):
+        print(f"{i}. {val} ${valorespastel[i]}")
+
+    opcion=int(input("Seleccione un producto(numero):"))
+    cantidad=int(input("Ingrese la cantidad que desea comprar: "))
+    total=valorespastel[opcion]*cantidad
+
+    print(f"Usted seleccionó el producto {Productospastel[opcion]} con un valor de ${total}")
+
+    dinero = int(input("Ingrese la cantidad de dinero disponible: "))
+    vuelto = dinero - total
+    
+    if dinero >= total:
+        print(f"Usted compró el producto {Productospastel[opcion]} con un valor de ${total}, sus vueltos son ${vuelto}")
+    else:
+        print(f"Le falta dinero, necesita ${-vuelto} más para comprar este producto.")
+
 
 
 
